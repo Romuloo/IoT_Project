cae39a8820e7f7bc8980525c835d27298bb4e0e8
Javier Linares Castrillón <j.linares5@usp.ceu.es>
2021-08-24 21:21:04 +0100

    I've added the js script

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from flask import Flask, render_template
-
+import json, time, threading
 
 app = Flask(__name__)
+
+alive = 0
+data = {} 
 
 
 @app.route('/')
 def index():
     return render_template("index.html");
+
+@app.route('/keep_alive', methods=["GET"])
+def keep_alive():
+    global alive, data
+    alive += 1
+    keep_alive_count = str(alive)
+    data['keep_alive'] = keep_alive_count
+    parsed_json = json.dumps(data)
+    print(str(parsed_json))
+    return str(parsed_json)
 
 
 if __name__ == '__main__':
---
+++ static/js/main.js
@@ -0,0 +1,41 @@
+var alive_second = 0;
+var heartbeat_rate = 5000;
+
+function keep_alive()
+{
+	var request  = new XMLHttpRequest();
+	request.onereadystatechange = function()
+	{
+		if(this.readyState === 4)
+		{
+			if(this.status === 200)
+			{
+				if(this.responseText != null)
+				{
+					var date = new Date();
+					alive_seccond = date.getTime();
+					var keep_alive_data = this.responseText;
+					console.log(keep_alive_data);
+				}
+			}
+		}
+	};
+	request.open("GET", "keep_alive", true);
+	request.send(null);
+	setTimeout('keep_alive()', heartbeat_rate);
+}
+
+function time()
+{
+	var d = new Date();
+	var current_sec = d.getTime();
+	if(current_sec - alive_second > heartbeat_rate + 1000)
+	{
+		document.getElementById("Connection_id").innerHTML = " Dead";
+	}
+	else
+	{
+		document.getElementById("Connection_id").innerHTML = " Alive";
+	}
+	setTimeout('time()', 1000);
+}
