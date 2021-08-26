ce14d7c911afc57ab492d57d4c85d7b6acd1b974
Javier Linares Castrillón <j.linares5@usp.ceu.es>
2021-08-26 22:22:46 +0100

    slightly changes done

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def tempHumidity():
         if data["works"]:
             humidity, temperature = Adafruit_DHT.read_retry(11,4)
             print('Temp:  {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity))
-            data["tempHum"] ='Temperature: ' + str(temperature) + ' | Humidity: ' + str(humidity)
+            data["tempHum"] ='Temperature: ' + str(temperature) + ' ºC'  + ' | Humidity: ' + str(humidity) + ' %'
             time.sleep(5)
 
 
